a93424a00dc0979848ce8ee025a1e66d74289363
IltafCoder <138275083+IltafCoder@users.noreply.github.com>
2023-07-02 19:24:41 +0500

    script.js

--- script.js
+++ script.js
@@ -1,34 +1,52 @@
 
-function iconColor(element) {
-
-    if (element == "user") {
-        let x = document.getElementById("user-icon")
-        x.style.color = "aqua"
-        x.style.marginLeft = "210px"
-        x.style.transition = "0.1s linear"
-    }
-    else {
-        let x = document.getElementById("pass-icon")
-        x.style.color = "aqua"
-        x.style.marginLeft = "210px"
-        x.style.transition = "0.1s linear"
-    }
+function iconColorSet(id) {
+    let x = document.getElementById(id)
+    x.style.color = "aqua"
+    x.style.marginLeft = "210px"
+    x.style.transition = "0.1s linear"
 }
 
+function iconColorReset(id) {
+    let x = document.getElementById(id)
+    x.style.color = "black"
+    x.style.marginLeft = "200px"
+    x.style.transition = "0.1s linear"
+}
 
-function iconColorReset(element) {
+function moveDivs(btn) {
 
-    if (element == "user") {
-        let x = document.getElementById("user-icon")
-        x.style.color = "black"
-        x.style.marginLeft = "200px"
-        x.style.transition = "0.1s linear"
+    if (btn == "join-btn") {
+        let x = document.getElementsByClassName("right-window")
+        let y = document.getElementsByClassName("left-window")
+        let z = document.getElementsByClassName("reg-form")
 
+        x[0].classList.add("move-div-right")
+        y[0].classList.add("move-div-left")
+
+        setTimeout(gr = () => {
+
+            x[0].style.display = 'none'
+            y[0].style.display = 'none'
+            z[0].style.display = 'flex'
+            z[0].classList.add("scale-up-div")
+        }, 800)
     }
-    else {
-        let x = document.getElementById("pass-icon")
-        x.style.color = "black"
-        x.style.marginLeft = "200px"
-        x.style.transition = "0.1s linear"
+    else if (btn == "login-btn") {
+
+        let x = document.getElementsByClassName("right-window")
+        let y = document.getElementsByClassName("left-window")
+        let z = document.getElementsByClassName("reg-form")
+        z[0].classList.add("scale-in-div")
+
+        setTimeout(gr = () => {
+
+            x[0].style.display = 'flex'
+            y[0].style.display = 'flex'
+            z[0].style.display = 'none'
+            z[0].classList.remove("scale-in-div")
+
+            x[0].classList.add("in-left")
+            y[0].classList.add("in-right")
+        }, 800)
     }
 }
